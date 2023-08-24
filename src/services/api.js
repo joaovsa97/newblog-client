4e4923a731d3b6f3153c5ecb0468e1f39fdec5b4
joaovsa97 <joaos.azevedo10@gmail.com>
2023-08-24 16:28:25 -0300

    feat: mobile layout

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const api = axios.create({
-    // baseURL: "https://newblog-api.onrender.com",
-    baseURL: "http://localhost:8800",
-    withCredentials: true
+    baseURL: "https://newblog-api.onrender.com",
+    // baseURL: "http://localhost:8800",
+    withCredentials: true,
+    Authorization: Cookies.get("access_token")
 })
